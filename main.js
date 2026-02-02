a51031a8dc4201261f0180c1ed00e1def801026e
NATHAN PROENÇA DE LUNA <25205657@cfp127aluno.local>
2026-02-02 17:00:24 -0300

    em andamento

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ function trocarCor(){
     document.documentElement.style.setProperty("--color-bg", corUsuario)
 }
 botaoTrocarCor.addEventListener("click", trocarCor)
+
+
+
+
+
+
+const black =document.getElementById("modo-black")
+const white =document.getElementById("modo-white")
+
+function modoescuro(){
+    const black = document.getElementById("modo-black").value
+    
+}
+function modoclaro(){
+
+}
+
+black.addEventListener("click", modoescuro) 
+white.addEventListener("click", trocarCor)
+
+
