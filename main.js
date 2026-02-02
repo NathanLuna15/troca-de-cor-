19cee7dd6efd3abf34e86b787409645c816c0487
NATHAN PROENÇA DE LUNA <25205657@cfp127aluno.local>
2026-02-02 16:11:50 -0300

    troca de cores em ingles

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strct"
+
+const botaoTrocarCor = document.getElementById("botao-trocar-cor")
+
+function trocarCor(){
+    const corUsuario = document.getElementById("cor-usuario").value
+    document.documentElement.style.setProperty("--color-bg", corUsuario)
+}
+botaoTrocarCor.addEventListener("click", trocarCor)
