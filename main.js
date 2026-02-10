9026dd2d54accdba3ab16f94b940b4dc6f866448
NATHAN PROENÇA DE LUNA <25205657@cfp127aluno.local>
2026-02-10 16:31:36 -0300

    modo black e white

--- main.js
+++ main.js
@@ -4,11 +4,21 @@ const botaoTrocarCor = document.getElementById("botao-trocar-cor")
 
 function trocarCor(){
     const corUsuario = document.getElementById("cor-usuario").value
-    document.documentElement.style.setProperty("--color-bg", corUsuario)
+    
+    if(corUsuario == "verde"){
+        document.documentElement.style.setProperty("--color-bg", "green")
+    }else if(corUsuario == "vermelho"){
+        document.documentElement.style.setProperty("--color-bg", "red")
+    }else if(corUsuario == "amarelo"){
+        document.documentElement.style.setProperty("--color-bg", "yellow")
+    }else if(corUsuario == "roxo"){
+        document.documentElement.style.setProperty("--color-bg", "purple")
+
+    }else{
+        document.documentElement.style.setProperty("--color-bg", corUsuario)
+    }
 }
 botaoTrocarCor.addEventListener("click", trocarCor)
-
-
 
 
 
@@ -17,14 +27,13 @@ const black =document.getElementById("modo-black")
 const white =document.getElementById("modo-white")
 
 function modoescuro(){
-    const black = document.getElementById("modo-black").value
-    
+    document.documentElement.style.setProperty("--color-bg", "black") 
 }
 function modoclaro(){
-
+    document.documentElement.style.setProperty("--color-bg", "white")
 }
 
 black.addEventListener("click", modoescuro) 
-white.addEventListener("click", trocarCor)
+white.addEventListener("click", modoclaro)
 
 
